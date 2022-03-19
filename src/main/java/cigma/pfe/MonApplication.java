68b211774211e96cefd38c279accd805b95f64f3
issamsarhi <aissamsarhi001@gmail.com>
2022-03-19 02:04:41 +0100

    TP7

---
+++ src/main/java/cigma/pfe/MonApplication.java
@@ -0,0 +1,16 @@
+package cigma.pfe;
+
+import cigma.pfe.controllers.ClientController;
+import cigma.pfe.models.Client;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MonApplication {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ClientController ctrl = (ClientController) context.getBean("controller");
+        Client client = new Client("MOUFTAH");
+        ctrl.save(client);
+    }
+}
